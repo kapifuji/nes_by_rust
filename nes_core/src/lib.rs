c8a8fe6cc775b1e28f32caf99dfcf93f29c1683b
rpianna <kantarui619816@gmail.com>
2022-11-08 00:40:56 +0900

    ROMを読み込む仕組みを実装

--- nes_core/src/lib.rs
+++ nes_core/src/lib.rs
@@ -1,1 +1,8 @@
+mod rom;
+pub struct Nes {}
 
+impl Nes {
+    pub fn new(rom_data: Vec<u8>) -> Nes {
+        Nes {}
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::prelude::*;
+use std::io::Result;
+
+fn read_rom(path: &str) -> Result<Vec<u8>> {
+    use std::fs::File;
+
+    let f = File::open(path)?;
+
+    let mut rom_bytes: Vec<u8> = Vec::new();
+    for byte in f.bytes() {
+        rom_bytes.push(byte.unwrap());
+    }
+    Ok(rom_bytes)
+}
+
+fn main() -> Result<()> {
+    use nes_core::Nes;
+
+    let rom = read_rom("./rom/sample1.nes")?;
+    let nes = Nes::new(rom);
+    Ok(())
 }
