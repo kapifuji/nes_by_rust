bb4f943c500719e01b09acc85472b2b0eb1ec8b0
rpianna <kantarui619816@gmail.com>
2022-12-19 01:04:22 +0900

    CPUの初期化処理を実装

--- nes_core/src/cpu.rs
+++ nes_core/src/cpu.rs
@@ -1,3 +1,4 @@
+#[derive(Default)]
 pub struct PpuRegister {
     ppu_control: u8,
     ppu_mask: u8,
@@ -8,13 +9,26 @@ pub struct PpuRegister {
     ppu_address: u8,
     ppu_data: u8,
 }
+
 struct CpuRegister {
     a: u8,
     x: u8,
     y: u8,
-    s: u8,
     p: StatusRegister,
-    pc: u8,
+    sp: u16,
+    pc: u16,
+}
+impl CpuRegister {
+    pub fn new() -> Self {
+        Self {
+            a: 0x00,
+            x: 0x00,
+            y: 0x00,
+            p: StatusRegister::new(),
+            sp: 0x01fd,
+            pc: 0x00,
+        }
+    }
 }
 
 struct StatusRegister {
@@ -36,6 +50,20 @@ struct StatusRegister {
     /// bit0 carry
     c: bool,
 }
+impl StatusRegister {
+    pub fn new() -> Self {
+        Self {
+            n: false,
+            v: false,
+            r: true,
+            b: true,
+            d: false,
+            i: true,
+            z: false,
+            c: false,
+        }
+    }
+}
 
 struct CpuMemory {
     /// 0x0000 ~ 0x07FF
@@ -45,6 +73,26 @@ struct CpuMemory {
     /// 0x8000 ~ 0xBFFF
     prg_rom: [u8; 0x8000],
 }
+impl CpuMemory {
+    pub fn new(prg_rom_data: &Vec<u8>) -> Self {
+        let wram: [u8; 0x800] = [0; 0x800];
+        let ppu_register = PpuRegister::default();
+        let mut prg_rom: [u8; 0x8000] = [0; 0x8000];
+
+        for i in 0..prg_rom.len() {
+            if i >= prg_rom_data.len() {
+                break;
+            }
+            prg_rom[i] = prg_rom_data[i];
+        }
+
+        Self {
+            wram,
+            ppu_register,
+            prg_rom,
+        }
+    }
+}
 
 pub struct Cpu {
     register: CpuRegister,
@@ -53,13 +101,37 @@ pub struct Cpu {
 
 impl Cpu {
     pub fn new(prg_rom_data: &Vec<u8>) -> Cpu {
-        todo!()
+        let register = CpuRegister::new();
+        let memory_map = CpuMemory::new(&prg_rom_data);
+
+        Self {
+            register,
+            memory_map,
+        }
     }
+
     pub fn tick() {
         todo!()
     }
 
     pub fn get_ppu_register() -> PpuRegister {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn cpu_new() {
+        let mut prg_rom: Vec<u8> = Vec::new();
+        for i in 0..5 {
+            prg_rom.push(i);
+        }
+        let cpu = Cpu::new(&prg_rom);
+
+        assert_eq!(cpu.register.sp, 0x01fd);
+        assert_eq!(cpu.memory_map.prg_rom[0..5], [0x00, 0x01, 0x02, 0x03, 0x04]);
     }
 }
