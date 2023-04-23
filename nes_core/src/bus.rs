f97418b44d9d8f20e71480b3668fe40298236224
rpianna <kantarui619816@gmail.com>
2023-04-24 00:57:56 +0900

    PPUへの読み書き実装

--- nes_core/src/bus.rs
+++ nes_core/src/bus.rs
@@ -29,7 +29,7 @@ impl Bus {
         self.program[address as usize]
     }
 
-    pub fn read_memory_byte(&self, address: u16) -> u8 {
+    pub fn read_memory_byte(&mut self, address: u16) -> u8 {
         match address {
             0..=0x1fff => {
                 // 0x0000 ~ 0x1fff は 0x0000 ~ 0x07ff のみ有効。（以降はミラーリング）
@@ -37,18 +37,25 @@ impl Bus {
                 let address = address & (0b0000_0111_1111_1111);
                 self.wram[address as usize]
             }
-            0x2000..=0x3fff => {
-                // 0x2000 ~ 0x3fff は 0x2000 ~ 0x2008 のみ有効。（以降はミラーリンク）
+            0x2000 | 0x2001 | 0x2003 | 0x2005 | 0x2006 | 0x4014 => {
+                panic!("{} は書き込み専用のPPUアドレスです。", address)
+            }
+            0x2007 => self.ppu.read_data(),
+            0x2008..=0x3fff => {
+                // 0x2000 ~ 0x3fff は 0x2000 ~ 0x2008 のみ有効。（以降はミラーリング）
                 // よって、bit0 ~ bit2, bit13 を mask
                 let address = address & (0b0010_0000_0000_0111);
-                todo!()
+                self.read_memory_byte(address)
             }
             0x8000..=0xffff => self.read_program_rom_byte(address),
-            _ => panic!("unexpected address: {}", address),
+            _ => {
+                println!("{} へのbyteリードアクセスは無視されます。", address);
+                0
+            }
         }
     }
 
-    pub fn read_memory_word(&self, address: u16) -> u16 {
+    pub fn read_memory_word(&mut self, address: u16) -> u16 {
         match address {
             0..=0x1ffe => {
                 // 0x0000 ~ 0x1fff は 0x0000 ~ 0x07ff のみ有効。（以降はミラーリング）
@@ -58,39 +65,40 @@ impl Bus {
                 let hi = self.read_memory_byte(address + 1);
                 (lo as u16) + ((hi as u16) << 8)
             }
-            0x2000..=0x3ffe => {
-                // 0x2000 ~ 0x3fff は 0x2000 ~ 0x2008 のみ有効。（以降はミラーリンク）
-                // よって、bit0 ~ bit2, bit13 を mask
-                let address = address & (0b0010_0000_0000_0111);
-                todo!()
-            }
             0x8000..=0xfffe => {
                 let lo = self.read_program_rom_byte(address);
                 let hi = self.read_program_rom_byte(address + 1);
                 (lo as u16) + ((hi as u16) << 8)
             }
-            _ => panic!("unexpected address: {}", address),
+            _ => {
+                println!("{} へのwordリードアクセスは無視されます。", address);
+                0
+            }
         }
     }
 
     pub fn write_memory_byte(&mut self, address: u16, value: u8) {
-        let write_ref = match address {
+        match address {
             0..=0x1fff => {
                 // 0x0000 ~ 0x1fff は 0x0000 ~ 0x07ff のみ有効。（以降はミラーリング）
                 // よって、bit0 ~ 10 を mask
                 let address = address & (0b0000_0111_1111_1111);
-                &mut self.wram[address as usize]
+                self.wram[address as usize] = value;
             }
-            0x2000..=0x3fff => {
+            0x2000 => self.ppu.write_to_control(value),
+            0x2006 => self.ppu.write_to_ppu_address(value),
+            0x2007 => self.ppu.write_to_data(value),
+            0x2008..=0x3fff => {
                 // 0x2000 ~ 0x3fff は 0x2000 ~ 0x2008 のみ有効。（以降はミラーリンク）
                 // よって、bit0 ~ bit2, bit13 を mask
                 let address = address & (0b0010_0000_0000_0111);
-                todo!()
+                self.write_memory_byte(address, value);
             }
             0x8000..=0xffff => panic!("ROM is readonly"),
-            _ => panic!("unexpected address: {}", address),
+            _ => {
+                println!("{} へのbyteライトアクセスは無視されます。", address);
+            }
         };
-        *write_ref = value;
     }
 
     pub fn write_memory_word(&mut self, address: u16, value: u16) {
@@ -105,16 +113,16 @@ impl Bus {
                 self.write_memory_byte(address, lo);
                 self.write_memory_byte(address + 1, hi)
             }
-            0x2000..=0x3ffe => {
-                // 0x2000 ~ 0x3fff は 0x2000 ~ 0x2008 のみ有効。（以降はミラーリンク）
-                // よって、bit0 ~ bit2, bit13 を mask
-                let address = address & (0b0010_0000_0000_0111);
-                todo!()
-            }
             0x8000..=0xfffe => {
                 panic!("ROM is readonly")
             }
-            _ => panic!("unexpected address: {}", address),
+            _ => {
+                println!("{} へのwordライトアクセスは無視されます。", address);
+            }
         }
+    }
+
+    pub fn poll_nmi_status(&self) -> bool {
+        self.ppu.poll_nmi_status()
     }
 }
