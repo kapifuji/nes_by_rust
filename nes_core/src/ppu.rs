07fc70a0ffa14de4f2eddb3f33e8fb6fc2d45f4f
rpianna <kantarui619816@gmail.com>
2023-04-24 00:56:49 +0900

    PPUのレジスタ実装 PPUのNMI割り込み実装

--- nes_core/src/ppu.rs
+++ nes_core/src/ppu.rs
@@ -194,22 +194,32 @@ impl StatusRegister {
 
 #[derive(Default)]
 struct PpuRegister {
-    ppu_control: u8,
+    ppu_control: ControlRegister,
     ppu_mask: u8,
-    ppu_status: u8,
+    ppu_status: StatusRegister,
     oam_address: u8,
     oam_data: u8,
     ppu_scroll: u8,
-    ppu_address: u8,
+    ppu_address: AddressRegister,
     ppu_data: u8,
 }
 
 pub struct Ppu {
+    /// 0x0000 ~ 0x1FFF
     charactor_rom: Vec<u8>,
+    /// 0x3F00 ~ 0x3FFF
     palette_table: [u8; 0x20],
+    /// 0x2000 ~ 0x3F00
     vram: [u8; 0x800],
     oam_data: [u8; 0x100],
     mirroring: Mirroring,
+    register: PpuRegister,
+    internal_data_buf: u8,
+
+    scanline: u16,
+    cycles: usize,
+
+    nmi_interrupt: bool,
 }
 
 impl Ppu {
@@ -220,6 +230,151 @@ impl Ppu {
             vram: [0; 0x800],
             oam_data: [0; 0x100],
             mirroring: mirroring,
+            register: PpuRegister::default(),
+            internal_data_buf: 0,
+
+            scanline: 0,
+            cycles: 0,
+            nmi_interrupt: false,
+        }
+    }
+
+    pub fn tick(&mut self, cycles: u8) -> bool {
+        self.cycles += cycles as usize;
+        // 1 scanline あたり、341 PPU clock
+        if self.cycles >= 341 {
+            self.cycles -= 341;
+            self.scanline += 1;
+
+            // 241 scanline の時点でNMI割り込みをトリガーする。
+            if self.scanline == 241 {
+                self.register.ppu_status.set_vertical_blank();
+                if self.register.ppu_control.read_generate_nmi() {
+                    self.nmi_interrupt = true;
+                }
+            }
+
+            // PPU は 262 scanline をレンダリングする。
+            if self.scanline >= 262 {
+                self.scanline = 0;
+                self.nmi_interrupt = false;
+                self.register.ppu_status.reset_vertical_blank();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    pub fn poll_nmi_status(&self) -> bool {
+        self.register.ppu_control.read_generate_nmi()
+    }
+
+    pub fn write_to_ppu_address(&mut self, value: u8) {
+        self.register.ppu_address.update(value);
+    }
+
+    pub fn write_to_control(&mut self, value: u8) {
+        let before_nmi_status = self.register.ppu_control.read_generate_nmi();
+        self.register.ppu_control.update(value);
+        if !before_nmi_status
+            && self.register.ppu_control.read_generate_nmi()
+            && self.register.ppu_status.is_vertical_blank()
+        {
+            self.nmi_interrupt = true;
+        }
+    }
+
+    /// 事前に address レジスタにアドレスを書き込んでおく。
+    /// そのアドレスに対応したPPU内部のデータに書き込む。
+    /// 呼び出すたびに、アドレスがインクリメントされる。
+    pub fn write_to_data(&mut self, value: u8) {
+        let address = self.register.ppu_address.get();
+        self.increment_vram_address();
+
+        match address {
+            0..=0x1fff => {
+                self.charactor_rom[address as usize] = value;
+            }
+            0x2000..=0x2fff => {
+                self.vram[self.mirror_vram_address(address) as usize] = value;
+            }
+            0x3000..=0x3eff => panic!(
+                "アドレス 0x3000 ~ 0x3eff 使用を想定していません。 要求: {}",
+                address
+            ),
+            0x3f00..=0x3fff => self.palette_table[(address - 0x3f00) as usize] = value,
+            _ => panic!(
+                "ミラーリング領域への予期せぬライトアクセスです。 要求: {}",
+                address
+            ),
+        }
+    }
+
+    fn increment_vram_address(&mut self) {
+        self.register
+            .ppu_address
+            .increment(self.register.ppu_control.vram_address_increment());
+    }
+
+    /// 事前に address レジスタにアドレスを書き込んでおく。
+    /// そのアドレスに対応したPPU内部のデータを返す。
+    /// 呼び出すたびに、アドレスがインクリメントされる。
+    pub fn read_data(&mut self) -> u8 {
+        let address = self.register.ppu_address.get();
+        self.increment_vram_address();
+
+        match address {
+            0..=0x1fff => {
+                let result = self.internal_data_buf;
+                self.internal_data_buf = self.charactor_rom[address as usize];
+                result
+            }
+            0x2000..=0x2fff => {
+                let result = self.internal_data_buf;
+                self.internal_data_buf = self.vram[self.mirror_vram_address(address) as usize];
+                result
+            }
+            0x3000..=0x3eff => panic!(
+                "アドレス 0x3000 ~ 0x3eff 使用を想定していません。 要求: {}",
+                address
+            ),
+            0x3f00..=0x3fff => self.palette_table[(address - 0x3f00) as usize],
+            _ => panic!(
+                "ミラーリング領域への予期せぬリードアクセスです。 要求: {}",
+                address
+            ),
+        }
+    }
+
+    pub fn mirror_vram_address(&self, address: u16) -> u16 {
+        // 画面は2面ある。1面あたり0x400で、vramの0x800に収める。
+        // ただ、アドレス空間上は4面ある。（0x2000 ~ 0x3fffで、0x3000~は0x2000~にミラーリングする。）
+        // 下の例だと、左上、右上、左下、右下の順にマッピングされる。
+        // vramにはＡ, Bの順で格納する。a, bはそれぞれA, Bにミラーリングする。
+        //
+        // Horizontal(水平):
+        //   [ A ] [ a ]
+        //   [ B ] [ b ]
+        //
+        // Vertical(垂直):
+        //   [ A ] [ B ]
+        //   [ a ] [ b ]
+        //
+
+        // 0x3000 ~ 0x3eff を 0x2000 ~ 0x2eff にする。（ミラーリング）
+        let mirrored_vram = address & 0b0010_1111_1111_1111;
+        // vram index（0x2000始まりなので、計算のため減算。）
+        let vram_index = mirrored_vram - 0x2000;
+        // name table index（4面のうち、そのアドレス領域を指すか分かる。）
+        let name_table = vram_index / 0x400;
+
+        // ミラーリング指定に応じてミラーリング
+        match (&self.mirroring, name_table) {
+            (Mirroring::Vertical, 2) | (Mirroring::Vertical, 3) => vram_index - 0x800,
+            (Mirroring::Horizontal, 2) => vram_index - 0x400,
+            (Mirroring::Horizontal, 1) => vram_index - 0x400,
+            (Mirroring::Horizontal, 3) => vram_index - 0x800,
+            _ => vram_index,
         }
     }
 }
