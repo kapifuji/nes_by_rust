f9ed0dab417b6e718eb90f386abc8e6c8cd1069f
rpianna <kantarui619816@gmail.com>
2023-05-08 00:46:40 +0900

    main関数を仮実装

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,119 @@
 use std::io::prelude::*;
 use std::io::Result;
 
+use nes_core::cpu::Cpu;
+use nes_core::ppu::Ppu;
+use nes_core::rom::Rom;
+use rand::Rng;
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::pixels::PixelFormatEnum;
+use sdl2::EventPump;
+
+#[rustfmt::skip]
+pub static SYSTEM_PALLETE: [(u8,u8,u8); 64] = [
+   (0x80, 0x80, 0x80), (0x00, 0x3D, 0xA6), (0x00, 0x12, 0xB0), (0x44, 0x00, 0x96), (0xA1, 0x00, 0x5E),
+   (0xC7, 0x00, 0x28), (0xBA, 0x06, 0x00), (0x8C, 0x17, 0x00), (0x5C, 0x2F, 0x00), (0x10, 0x45, 0x00),
+   (0x05, 0x4A, 0x00), (0x00, 0x47, 0x2E), (0x00, 0x41, 0x66), (0x00, 0x00, 0x00), (0x05, 0x05, 0x05),
+   (0x05, 0x05, 0x05), (0xC7, 0xC7, 0xC7), (0x00, 0x77, 0xFF), (0x21, 0x55, 0xFF), (0x82, 0x37, 0xFA),
+   (0xEB, 0x2F, 0xB5), (0xFF, 0x29, 0x50), (0xFF, 0x22, 0x00), (0xD6, 0x32, 0x00), (0xC4, 0x62, 0x00),
+   (0x35, 0x80, 0x00), (0x05, 0x8F, 0x00), (0x00, 0x8A, 0x55), (0x00, 0x99, 0xCC), (0x21, 0x21, 0x21),
+   (0x09, 0x09, 0x09), (0x09, 0x09, 0x09), (0xFF, 0xFF, 0xFF), (0x0F, 0xD7, 0xFF), (0x69, 0xA2, 0xFF),
+   (0xD4, 0x80, 0xFF), (0xFF, 0x45, 0xF3), (0xFF, 0x61, 0x8B), (0xFF, 0x88, 0x33), (0xFF, 0x9C, 0x12),
+   (0xFA, 0xBC, 0x20), (0x9F, 0xE3, 0x0E), (0x2B, 0xF0, 0x35), (0x0C, 0xF0, 0xA4), (0x05, 0xFB, 0xFF),
+   (0x5E, 0x5E, 0x5E), (0x0D, 0x0D, 0x0D), (0x0D, 0x0D, 0x0D), (0xFF, 0xFF, 0xFF), (0xA6, 0xFC, 0xFF),
+   (0xB3, 0xEC, 0xFF), (0xDA, 0xAB, 0xEB), (0xFF, 0xA8, 0xF9), (0xFF, 0xAB, 0xB3), (0xFF, 0xD2, 0xB0),
+   (0xFF, 0xEF, 0xA6), (0xFF, 0xF7, 0x9C), (0xD7, 0xE8, 0x95), (0xA6, 0xED, 0xAF), (0xA2, 0xF2, 0xDA),
+   (0x99, 0xFF, 0xFC), (0xDD, 0xDD, 0xDD), (0x11, 0x11, 0x11), (0x11, 0x11, 0x11)
+];
+
+pub struct Frame {
+    pub data: Vec<u8>,
+}
+
+impl Frame {
+    const WIDTH: usize = 256;
+    const HEIGHT: usize = 240;
+
+    pub fn new() -> Self {
+        Frame {
+            data: vec![0; Frame::WIDTH * Frame::HEIGHT * 3],
+        }
+    }
+
+    pub fn set_pixel(&mut self, x: usize, y: usize, rgb: (u8, u8, u8)) {
+        let base = y * 3 * Frame::WIDTH + x * 3;
+        if base + 2 < self.data.len() {
+            self.data[base] = rgb.0;
+            self.data[base + 1] = rgb.1;
+            self.data[base + 2] = rgb.2;
+        }
+    }
+}
+
+pub fn render(ppu: &nes_core::ppu::Ppu, frame: &mut Frame) {
+    let bank = ppu.read_background_pattern_address();
+
+    for i in 0..0x03c0 {
+        // just for now, lets use the first nametable
+        let tile = ppu.vram[i] as u16;
+        let tile_x = i % 32;
+        let tile_y = i / 32;
+        let tile =
+            &ppu.charactor_rom[(bank + tile * 16) as usize..=(bank + tile * 16 + 15) as usize];
+
+        for y in 0..=7 {
+            let mut upper = tile[y];
+            let mut lower = tile[y + 8];
+
+            for x in (0..=7).rev() {
+                let value = (1 & upper) << 1 | (1 & lower);
+                upper = upper >> 1;
+                lower = lower >> 1;
+                let rgb = match value {
+                    0 => SYSTEM_PALLETE[0x01],
+                    1 => SYSTEM_PALLETE[0x23],
+                    2 => SYSTEM_PALLETE[0x27],
+                    3 => SYSTEM_PALLETE[0x30],
+                    _ => panic!("can't be"),
+                };
+                frame.set_pixel(tile_x * 8 + x, tile_y * 8 + y, rgb)
+            }
+        }
+    }
+}
+
+fn show_tile(chr_rom: &Vec<u8>, bank: usize, tile_n: usize) -> Frame {
+    assert!(bank <= 1);
+
+    let mut frame = Frame::new();
+    let bank = (bank * 0x1000) as usize;
+
+    let tile = &chr_rom[(bank + tile_n * 16)..=(bank + tile_n * 16 + 15)];
+
+    for y in 0..=7 {
+        let mut upper = tile[y];
+        let mut lower = tile[y + 8];
+
+        for x in (0..=7).rev() {
+            let value = (1 & upper) << 1 | (1 & lower);
+            upper = upper >> 1;
+            lower = lower >> 1;
+            let rgb = match value {
+                0 => SYSTEM_PALLETE[0x01],
+                1 => SYSTEM_PALLETE[0x23],
+                2 => SYSTEM_PALLETE[0x27],
+                3 => SYSTEM_PALLETE[0x30],
+                _ => panic!("can't be"),
+            };
+            frame.set_pixel(x, y, rgb)
+        }
+    }
+
+    frame
+}
+
 fn read_rom(path: &str) -> Result<Vec<u8>> {
     use std::fs::File;
 
@@ -13,10 +126,158 @@ fn read_rom(path: &str) -> Result<Vec<u8>> {
     Ok(rom_bytes)
 }
 
-fn main() -> Result<()> {
-    use nes_core::Nes;
+fn handle_user_input(cpu: &mut nes_core::cpu::Cpu, event_pump: &mut EventPump) {
+    for event in event_pump.poll_iter() {
+        match event {
+            Event::Quit { .. }
+            | Event::KeyDown {
+                keycode: Some(Keycode::Escape),
+                ..
+            } => std::process::exit(0),
+            Event::KeyDown {
+                keycode: Some(Keycode::W),
+                ..
+            } => {
+                cpu.write_memory_byte(0xff, 0x77);
+            }
+            Event::KeyDown {
+                keycode: Some(Keycode::S),
+                ..
+            } => {
+                cpu.write_memory_byte(0xff, 0x73);
+            }
+            Event::KeyDown {
+                keycode: Some(Keycode::A),
+                ..
+            } => {
+                cpu.write_memory_byte(0xff, 0x61);
+            }
+            Event::KeyDown {
+                keycode: Some(Keycode::D),
+                ..
+            } => {
+                cpu.write_memory_byte(0xff, 0x64);
+            }
+            _ => { /* do nothing */ }
+        }
+    }
+}
 
-    let rom = read_rom("./rom/sample1.nes")?;
-    let nes = Nes::new(rom);
+fn color(byte: u8) -> Color {
+    match byte {
+        0 => sdl2::pixels::Color::BLACK,
+        1 => sdl2::pixels::Color::WHITE,
+        2 | 9 => sdl2::pixels::Color::GREY,
+        3 | 10 => sdl2::pixels::Color::RED,
+        4 | 11 => sdl2::pixels::Color::GREEN,
+        5 | 12 => sdl2::pixels::Color::BLUE,
+        6 | 13 => sdl2::pixels::Color::MAGENTA,
+        7 | 14 => sdl2::pixels::Color::YELLOW,
+        _ => sdl2::pixels::Color::CYAN,
+    }
+}
+
+fn read_screen_state(cpu: &mut nes_core::cpu::Cpu, frame: &mut [u8; 32 * 3 * 32]) -> bool {
+    let mut frame_idx = 0;
+    let mut is_update = false;
+    for i in 0x0200..0x600 {
+        let color_idx = cpu.read_memory_byte(i as u16);
+        let (b1, b2, b3) = color(color_idx).rgb();
+        if frame[frame_idx] != b1 || frame[frame_idx + 1] != b2 || frame[frame_idx + 2] != b3 {
+            frame[frame_idx] = b1;
+            frame[frame_idx + 1] = b2;
+            frame[frame_idx + 2] = b3;
+            is_update = true;
+        }
+        frame_idx += 3;
+    }
+    is_update
+}
+
+fn main() -> Result<()> {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+    let window = video_subsystem
+        .window("Tile viewer", (256.0 * 3.0) as u32, (240.0 * 3.0) as u32)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().present_vsync().build().unwrap();
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    canvas.set_scale(3.0, 3.0).unwrap();
+
+    let creator = canvas.texture_creator();
+    let mut texture = creator
+        .create_texture_target(PixelFormatEnum::RGB24, 256, 240)
+        .unwrap();
+
+    let rom_data = read_rom("./rom/Alter_Ego.nes")?;
+    let mut frame = Frame::new();
+
+    let mut nes = nes_core::Nes::new(&rom_data, move |ppu: &Ppu| {
+        render(ppu, &mut frame);
+        texture.update(None, &frame.data, 256 * 3).unwrap();
+
+        canvas.copy(&texture, None, None).unwrap();
+
+        canvas.present();
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => std::process::exit(0),
+                _ => { /* 何もしない */ }
+            }
+        }
+    });
+
+    nes.run_with_callback(move |_| {});
+    //nes.run_with_callback(move |cpu| println!("{}", trace(cpu).to_uppercase()));
+
     Ok(())
+}
+
+// nestest
+fn main_() -> Result<()> {
+    let rom_data = read_rom("./rom/nestest.nes")?;
+    let mut nes = nes_core::Nes::new(&rom_data, |_| {});
+
+    nes.cpu.register.pc = 0xc000; // nestest 専用設定
+
+    nes.run_with_callback(move |cpu| println!("{}", trace(cpu).to_uppercase()));
+
+    Ok(())
+}
+
+fn trace(cpu: &mut Cpu) -> String {
+    let code = cpu.read_memory_byte(cpu.register.pc);
+    let opcode = cpu.opcodes.get(&code).expect("").clone();
+    let sub_code1 = if opcode.bytes > 1 {
+        format!("{:<02x}", cpu.read_memory_byte(cpu.register.pc + 1))
+    } else {
+        format!("  ")
+    };
+    let sub_code2 = if opcode.bytes > 2 {
+        format!("{:<02x}", cpu.read_memory_byte(cpu.register.pc + 2))
+    } else {
+        format!("  ")
+    };
+    let inst = opcode.instruction.to_string();
+
+    format!(
+        "{:<04x}  {:<02x} {} {} {}    A:{:<02x} X:{:<02x} Y:{:<02x} P:{:<02x} SP:{:<02x}",
+        cpu.register.pc,
+        code,
+        sub_code1,
+        sub_code2,
+        inst,
+        cpu.register.a,
+        cpu.register.x,
+        cpu.register.y,
+        cpu.register.p.bits(),
+        cpu.register.sp,
+    )
 }
