0cc860be7824c7272da73f950dc18c8f68836ee3
rpianna <kantarui619816@gmail.com>
2023-05-08 00:45:56 +0900

    Nes構造体を整理（暫定）

--- nes_core/src/lib.rs
+++ nes_core/src/lib.rs
@@ -1,27 +1,31 @@
 mod bus;
+pub mod cpu;
 mod opcode;
-mod ppu;
-mod rom;
+pub mod ppu;
+pub mod rom;
 
-pub struct Nes {
-    rom_header: rom::Header,
-    cpu: cpu::Cpu,
-    ppu: ppu::Ppu,
+pub struct Nes<'a> {
+    //rom_header: rom::Header,
+    pub cpu: cpu::Cpu<'a>,
+    //ppu: ppu::Ppu,
 }
 
-impl Nes {
-    pub fn new(rom_data: &Vec<u8>) -> Nes {
+impl<'a> Nes<'a> {
+    pub fn new<'b, F>(rom_data: &Vec<u8>, gameloop_callback: F) -> Nes<'b>
+    where
+        F: FnMut(&ppu::Ppu) + 'b,
+    {
         let rom = rom::Rom::new(rom_data);
+        let bus = bus::Bus::new(&rom, gameloop_callback);
         Nes {
-            cpu: cpu::Cpu::new(&rom),
+            cpu: cpu::Cpu::new(bus),
         }
     }
 
-    pub fn tick_cpu() {
-        todo!()
-    }
-
-    pub fn tick_ppu() {
-        todo!()
+    pub fn run_with_callback<F>(&mut self, callback: F)
+    where
+        F: FnMut(&mut cpu::Cpu),
+    {
+        self.cpu.run_with_callback(callback);
     }
 }
